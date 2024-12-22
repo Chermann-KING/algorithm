3ac9f714da4ad5d213c400895cfd8cec6847051e
Chermann-KING <codes.smiling@gmail.com>
2024-12-22 13:27:10 +0100

    ✅ Auth: Configuration du seed et mise en place d'un utilisateur test

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { authConfig } from "@/auth/auth-options";
+import { handlers } from "@/auth/auth-options";
+
+export const { GET, POST } = handlers;
